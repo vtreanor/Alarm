148de26d18d5dba0cdcbad18a973b50838e3f285
vincent <vincent@GOLDMINE-II>
2016-04-30 19:20:10 +0100

    Add project files.

---
+++ Alarm/mainForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Alarm
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.process1 = new System.Diagnostics.Process();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Arguments = "/query /XML /TN reminder";
+            this.process1.StartInfo.CreateNoWindow = true;
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.FileName = "C:\\Windows\\System32\\schtasks.exe";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.RedirectStandardOutput = true;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.StartInfo.UseShellExecute = false;
+            this.process1.StartInfo.WorkingDirectory = "C:\\Users\\vincent";
+            this.process1.SynchronizingObject = this;
+            // 
+            // timePicker
+            // 
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.timePicker.Location = new System.Drawing.Point(12, 12);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.ShowUpDown = true;
+            this.timePicker.Size = new System.Drawing.Size(96, 20);
+            this.timePicker.TabIndex = 10;
+            // 
+            // datePicker
+            // 
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePicker.Location = new System.Drawing.Point(126, 12);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(140, 20);
+            this.datePicker.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBar,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 43);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(380, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 14;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(39, 17);
+            this.statusBar.Text = "Status";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Margin = new System.Windows.Forms.Padding(50, 3, 0, 2);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(88, 17);
+            this.toolStripStatusLabel1.Text = "no-paper.co.uk";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 65);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.timePicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Alarm Clock";
+            this.TopMost = true;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Diagnostics.Process process1;
+        private System.Media.SoundPlayer player;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
---
+++ Alarm/mainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace Alarm
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            timePicker.Format = DateTimePickerFormat.Custom;
+            timePicker.CustomFormat = "HH:mm";
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+
+        //    process1.Start();
+
+        //    // To avoid deadlocks, always read the output stream first and then wait.
+        //    string output = process1.StandardOutput.ReadToEnd();
+        //    process1.WaitForExit();
+        //    //textBox1.Text = output;
+
+        //    System.IO.TextReader rdr = new StringReader(output);
+        //    XmlReader reader = XmlReader.Create(rdr);
+        //    try
+        //    {
+        //        while (reader.Read())
+        //        {
+        //            if (reader.NodeType == XmlNodeType.Element)
+        //            {
+        //                if (reader.Name == "UserId") { textBox1.Text += reader.ReadString(); textBox1.Text += "\r\n"; }
+        //                if (reader.Name == "Date") { textBox1.Text += reader.ReadString(); textBox1.Text += "\r\n"; }
+        //                if (reader.Name == "Command") { textBox1.Text += reader.ReadString(); textBox1.Text += "\r\n\r\n"; }
+
+        //            }
+        //        }
+        //    }
+        //    catch (System.Xml.XmlException exc)
+        //    {
+        //        String str = exc.Message;
+        //        Console.WriteLine(str);
+        //        //Console.WriteLine(e.Message);
+        //        //Console.WriteLine("Exception object Line, pos: (" + e.LineNumber + "," + e.LinePosition + ")");
+        //        //Console.WriteLine("XmlReader Line, pos: (" + tr.LineNumber + "," + tr.LinePosition + ")");
+
+        //    }
+
+        //}
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+            
+        //    if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        //    {
+        //        string path = openFileDialog1.FileName;
+        //        string xml = File.ReadAllText(path);
+                
+        //        System.IO.TextReader rdr = new StringReader(xml);
+        //        XmlReader reader = XmlReader.Create(rdr);                
+        //        try
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                if (reader.NodeType == XmlNodeType.Element)
+        //                {
+        //                    if (reader.Name == "UserId") { textBox1.Text += reader.ReadString(); textBox1.Text += "\r\n"; }
+        //                    if (reader.Name == "Date") { textBox1.Text += reader.ReadString(); textBox1.Text += "\r\n"; }
+        //                    if (reader.Name == "Command") { textBox1.Text += reader.ReadString(); textBox1.Text += "\r\n\r\n"; }
+
+        //                }
+        //            }
+        //        }
+        //        catch (System.Xml.XmlException exc)
+        //        {
+
+        //            String str = exc.Message;
+        //            Console.WriteLine(str);
+        //            //Console.WriteLine(e.Message);
+        //            //Console.WriteLine("Exception object Line, pos: (" + e.LineNumber + "," + e.LinePosition + ")");
+        //            //Console.WriteLine("XmlReader Line, pos: (" + tr.LineNumber + "," + tr.LinePosition + ")");
+
+        //        }
+
+
+
+        //    }
+
+
+        //}
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Boolean deleteOk = false;
+            Boolean createOk = false;
+            string output;
+            string task = @"c:\users\vincent\beep.bat";
+            string process = @"C:\Windows\System32\schtasks.exe";
+            string deleteStr = "/Delete /TN Alarm /F ";
+            string createStr = "/Create /SC ONCE /TN Alarm /TR " + task + " /ST " + timePicker.Text + " /SD " + datePicker.Text;
+            statusBar.Text = "Alarm at " + timePicker.Text + " on " + datePicker.Text; 
+            //textBox1.Text += deleteStr;
+            //textBox1.Text += "\r\n";
+            //textBox1.Text += createStr;
+            //textBox1.Text += "\r\n";
+
+            process1.StartInfo.FileName  = process;
+            process1.StartInfo.Arguments = deleteStr;
+
+            process1.Start();
+            output = process1.StandardOutput.ReadToEnd();
+            process1.WaitForExit();
+            deleteOk = output.Contains("SUCCESS");
+//----------------------------------------------------------------------------------
+            process1.StartInfo.FileName = process;
+            process1.StartInfo.Arguments = createStr;
+
+            process1.Start();
+            output = process1.StandardOutput.ReadToEnd();
+            process1.WaitForExit();
+            createOk = output.Contains("SUCCESS");
+            statusBar.Text += (deleteOk && createOk) ? " Success" : " Error";
+        }
+    }
+}
